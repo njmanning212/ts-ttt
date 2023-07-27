02114f619d831262aaa6e10c1824c4b927b57602
Nicholas Manning <njmanning212@gmail.com>
2023-07-27 13:45:09 -0500

    set constant of win combos and cache element references

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 "use strict";
+//constants
+const winCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 //variables
 let board, turn, winner, tie;
+//cached element references
+const squareEls = document.querySelectorAll('.sqr');
+const msgEl = document.getElementById('message');
+const resetBtnEl = document.querySelector('button');
+const boardEl = document.querySelector('.board');
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,23 @@
+//constants
+
+const winCombos: number[][] = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8], 
+    [0,4,8],
+    [2,4,6]
+]
+
 //variables
 
-let board: number[],turn: number, winner: boolean, tie: boolean
+let board: number[], turn: number, winner: boolean, tie: boolean
+
+//cached element references
+
+const squareEls = document.querySelectorAll('.sqr') as NodeListOf<HTMLDivElement>
+const msgEl = document.getElementById('message') as HTMLDivElement
+const resetBtnEl = document.querySelector('button') as HTMLButtonElement
+const boardEl = document.querySelector('.board') as HTMLDivElement
